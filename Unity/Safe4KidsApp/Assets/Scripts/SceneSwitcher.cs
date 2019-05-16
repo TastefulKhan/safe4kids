0bd27d275caf0fad1256e416c3508dab5630d40b
TastefulKhan <Marko.Vasev@gmail.com>
2019-05-16 20:44:30 +0800

    Game Running
    Hard Mode Only. Menu. Random Cards. Basic.
    
    Needs to be put into a grid, Jarrryd knows how to do that. Please send AUD $1k Thanks

---
+++ Unity/Safe4KidsApp/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoHardScene()
+    {
+        SceneManager.LoadScene("Hard");
+    }
+
+    public void GotoEasyScene()
+    {
+        SceneManager.LoadScene("Easy");
+    }
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
